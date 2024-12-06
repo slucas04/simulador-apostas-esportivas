f505cfe92fe15b9400a0200b053fdaa2bbf23642
Samuel <slucas0730@gmail.com>
2024-12-05 23:34:15 -0300

    Atualização API e FRONT

--- project.clj
+++ project.clj
@@ -1,10 +1,18 @@
 (defproject av3funcional "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Projeto para interação com API utilizando Clojure"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"][org.clojure/clojure "1.10.1"]
-               [clj-http "3.12.3"][cheshire "5.10.0"][org.clojure/data.json "2.5.0"][environ "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [clj-http "3.12.3"]
+                 [cheshire "5.10.1"]
+                 [org.clojure/data.json "2.5.0"]
+                 [environ "1.2.0"]
+                 [ring/ring-core "1.9.6"]
+                 [ring-cors "0.1.13"]
+                 [ring/ring-jetty-adapter "1.9.6"]
+                 [ring/ring-json "0.5.1"] ;; Middleware JSON
+                 [compojure "1.6.3"]]
   :main ^:skip-aot av3funcional.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/av3funcional/core.clj
+++ src/av3funcional/core.clj
@@ -1,106 +1,30 @@
 (ns av3funcional.core
   (:require [clj-http.client :as client]
             [cheshire.core :as json]
-            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.middleware.cors :refer [wrap-cors]]
+            [compojure.core :refer [defroutes POST GET PUT]]
             [clojure.string :as str]
             [environ.core :refer [env]]))
 
-(def api-key (:api-key env))
+(def saldo (atom 0.0))
+(def apostas (atom []))
+(def api-key (env :api-key))
 
-;; Função para carregar o saldo do arquivo
-(defn carregar-saldo []
-  (try
-    (if (.exists (io/file "saldo.txt"))
-      (let [conteudo (slurp "saldo.txt")]
-        (Double/parseDouble conteudo))
-      0.0) ;; Retorna 0.0 se o arquivo não existir
-    (catch Exception e
-      (println "Erro ao carregar o saldo. Inicializando com 0.")
-      0.0)))
-
-;; Função para salvar o saldo no arquivo
-(defn salvar-saldo [saldo]
-  (spit "saldo.txt" (str saldo))
-  (println "Saldo salvo no arquivo saldo.txt."))
-
-;; Funções de saque e depósito
-(defn sacar [saldo]
-  (println "Qual o valor do saque?")
-  (let [input (str/trim (read-line))]
-    (try
-      (let [saque (Double/parseDouble input)]
-        (if (>= @saldo saque)
-          (do
-            (swap! saldo - saque)
-            (println "Saque realizado com sucesso. Seu saldo atual é:" @saldo))
-          (println "Saldo insuficiente! Seu saldo atual é:" @saldo)))
-      (catch Exception e
-        (println "Valor inválido. Por favor, insira um número válido.")))))
-
-(defn depositar [saldo]
-  (println "Qual o valor do depósito?")
-  (let [input (str/trim (read-line))]
-    (try
-      (let [deposito (Double/parseDouble input)]
-        (if (> deposito 0)
-          (do
-            (swap! saldo + deposito)
-            (println "Depósito realizado com sucesso. Seu saldo atual é:" @saldo))
-          (println "O valor do depósito deve ser maior que 0.")))
-      (catch Exception e
-        (println "Valor inválido. Por favor, insira um número válido.")))))
-
-(defn checar-saldo [saldo]
-  (println "Seu saldo atual é:" @saldo))
-
-(defn gerenciar-conta [saldo]
-  (println "\n. . . . . . . . . . .")
-  (println "1. Realizar saque")
-  (println "2. Realizar depósito")
-  (println "3. Checar saldo")
-  (println "4. Voltar")
-  (let [opcao (str/trim (read-line))]
-    (cond
-      (= opcao "1") (do (sacar saldo) (gerenciar-conta saldo))
-      (= opcao "2") (do (depositar saldo) (gerenciar-conta saldo))
-      (= opcao "3") (do (checar-saldo saldo) (gerenciar-conta saldo))
-      (= opcao "4") nil
-      :else (do (println "Opção inválida! Por favor, escolha novamente.") (gerenciar-conta saldo)))))
-
-;; Buscar eventos de futebol
-(defn buscar-eventos [url params]
-  (try
-    (let [response (client/get url {:headers {:x-rapidapi-key api-key
-                                              :x-rapidapi-host "betano.p.rapidapi.com"}
-                                    :query-params params})
-          data (json/parse-string (:body response) true)]
-      data)
-    (catch Exception e
-      (println "Erro ao acessar a API:" (.getMessage e))
-      nil)))
-
-(defn listar-eventos [torneio-id]
+(defn buscar-eventos [torneio-id]
   (let [url "https://betano.p.rapidapi.com/events"
-        params {:tournamentId torneio-id}
-        data (buscar-eventos url params)]
-    (if data
-      (do
-        (println "Informações do Torneio:")
-        (println "Categoria:" (:categoryName data))
-        (println "Nome:" (:name data))
-        (println "ID do Torneio:" (:tournamentId data))
-        (println "\nEventos:")
-        (doseq [event (vals (:events data))]
-          (println "-------------------")
-          (println "Participante 1:" (:participant1 event))
-          (println "Participante 2:" (:participant2 event))
-          (println "Data:" (:date event))
-          (println "Horário:" (:time event))
-          (println "Status:" (:eventStatus event))
-          (println "ID do Evento:" (:eventId event))))
-      (println "Erro ao listar eventos ou nenhum evento encontrado."))))
+        headers {:x-rapidapi-key api-key
+                 :x-rapidapi-host "betano.p.rapidapi.com"}
+        params {:tournamentId torneio-id}]
+    (try
+      (let [response (client/get url {:headers headers :query-params params})
+            data (json/parse-string (:body response) true)]
+        data)
+      (catch Exception e
+        (println "Erro ao acessar a API de eventos:" (.getMessage e))
+        nil))))
 
-;; Buscar odds de um evento
 (defn buscar-odds [evento-id]
   (let [url "https://betano.p.rapidapi.com/odds_betano"
         headers {:x-rapidapi-key api-key
@@ -109,101 +33,111 @@
                 :oddsFormat "decimal"
                 :raw "false"}]
     (try
-      (let [response (client/get url {:headers headers :query-params params})
-            odds-data (json/parse-string (:body response) true)]
-        odds-data)
+      (let [response (client/get url {:headers headers :query-params params})]
+        (println "Resposta completa:" response)
+        (json/parse-string (:body response) true))
       (catch Exception e
-        (println "Erro ao acessar a API:" (.getMessage e))
+        (println "Erro ao acessar a API de odds:" (.getMessage e))
         nil))))
 
-;; Salvar aposta em arquivo
-(defn salvar-aposta [evento-id mercado-id resultado valor saldo-atual]
-  (let [linha (str "Evento: " evento-id ", Mercado: " mercado-id ", Resultado: " resultado ", Valor Apostado: " valor ", Saldo Após Aposta: " saldo-atual "\n")]
-    (spit "apostas.txt" linha :append true)
-    (println "Aposta salva com sucesso no arquivo apostas.txt.")))
+(defroutes app-routes
+  (GET "/saldo" []
+       (json/generate-string {:saldo @saldo}))
 
-;; Exibir odds e mercados com opção de apostar
-(defn exibir-odds [evento-id saldo]
-  (let [odds-data (buscar-odds evento-id)]
-    (if odds-data
-      (let [mercados (:markets odds-data)]
-        (println "-------------------")
-        (println "Times:" (:participant1 odds-data) "x" (:participant2 odds-data))
-        (println "Data:" (:date odds-data))
-        (println "Horário:" (:time odds-data))
-        (println "Status:" (:eventStatus odds-data))
-        (println "\nMercados Disponíveis:")
-        (doseq [[market-id market-data] mercados]
-          (println "Mercado ID:" market-id)
-          (doseq [[outcome-id outcome] (:outcomes market-data)]
-            (println " - Resultado:" (:outcomeName outcome)
-                     "Preço:" (get-in outcome [:bookmakers :betano :price]))))
-        ;; Perguntar se deseja apostar
-        (println "\nDeseja apostar em algum mercado? (s/n)")
-        (let [resposta (str/trim (read-line))]
-          (if (= resposta "s")
-            (do
-              (println "Digite o ID do mercado:")
-              (let [mercado-id (str/trim (read-line))]
-                (println "Digite o resultado desejado:")
-                (let [resultado (str/trim (read-line))]
-                  (println "Digite o valor da aposta:")
-                  (let [input (str/trim (read-line))]
-                    (try
-                      (let [valor-aposta (Double/parseDouble input)]
-                        (if (and (number? valor-aposta) (> valor-aposta 0) (>= @saldo valor-aposta))
-                          (do
-                            (swap! saldo - valor-aposta)
-                            (salvar-aposta evento-id mercado-id resultado valor-aposta @saldo)
-                            (println "Aposta realizada com sucesso!"))
-                          (println "Saldo insuficiente ou valor inválido para realizar a aposta.")))
-                      (catch Exception e
-                        (println "Valor inválido. Por favor, insira um número válido.")))))))
-          (println "Aposta não realizada.")))
-      (println "Erro ao buscar odds ou evento não encontrado.")))))
+(POST "/depositar" {body :body}
+  (try
+    (println "Mapa completo recebido no /depositar:" body)
+    (println "Chaves disponíveis no body (com detalhes):" (map #(str %) (keys body)))
 
-;; Menu para apostas de futebol
-(defn menu-aposta-futebol [saldo]
-  (println "\n. . . . . . . . . . .")
-  (println "1. Escolher um jogo")
-  (println "2. Voltar")
-  (let [opcao (str/trim (read-line))]
-    (cond
-      (= opcao "1") (do
-                      (println "Digite o ID do evento que deseja consultar:")
-                      (let [evento-id (str/trim (read-line))]
-                        (if (not (str/blank? evento-id))
-                          (exibir-odds evento-id saldo)
-                          (println "ID do evento não pode estar vazio.")))
-                      (menu-aposta-futebol saldo))
-      (= opcao "2") nil
-      :else (do
-              (println "Opção inválida! Por favor, escolha novamente.")
-              (menu-aposta-futebol saldo)))))
 
-;; Menu principal
-(defn menu-principal [saldo]
-  (println "\n. . . . . . . . . . .")
-  (println "1. Gerenciar sua conta")
-  (println "2. Fazer apostas")
-  (println "3. Consultar resultados")
-  (println "4. Buscar informações do torneio de futebol")
-  (println "5. Buscar informações do torneio de basquete")
-  (println "6. Sair")
-  (let [opcao (str/trim (read-line))]
-    (cond
-      (= opcao "1") (do (gerenciar-conta saldo) (menu-principal saldo))
-      (= opcao "2") (do (menu-aposta-futebol saldo) (menu-principal saldo))
-      (= opcao "3") (println "Consultar resultados em desenvolvimento.") ;; Funcionalidade futura
-      (= opcao "4") (do (listar-eventos "325") (menu-principal saldo))
-      (= opcao "5") (do (listar-eventos "24135") (menu-principal saldo))
-      (= opcao "6") (do
-                      (salvar-saldo @saldo)
-                      (println "Saindo..."))
-      :else (do
-              (println "Opção inválida! Por favor, escolha novamente.")
-              (menu-principal saldo)))))
+    (let [valor (try
+                  (let [v (or (:valor body)
+                              (get body (keyword "valor"))
+                              (get body (keyword "namespace/valor")))] 
+                    (println "Valor extraído do mapa:" v "Tipo do valor:" (type v))
+                    (Double/parseDouble (str v))) 
+                  (catch Exception e
+                    (println "Erro ao converter valor para número:" (.getMessage e))
+                    nil))]
+      (println "Valor processado para depósito:" valor)
+      (if (and valor (> valor 0))
+        (do
+          (swap! saldo + valor)
+          (println "Depósito realizado com sucesso. Novo saldo:" @saldo)
+          (json/generate-string {:mensagem "Depósito realizado com sucesso" :saldo @saldo}))
+        (json/generate-string {:erro "Valor inválido para depósito"})))
+    (catch Exception e
+      (println "Erro ao processar o corpo da requisição:" (.getMessage e))
+      (json/generate-string {:erro "Erro ao processar requisição"}))))
 
-;; Inicializador do programa
-(def saldo (atom (carregar-saldo)))
-(menu-principal saldo)
+
+
+
+
+
+  (PUT "/sacar" {body :body}
+       (let [valor (try
+                     (Double/parseDouble (str (:valor body)))
+                     (catch Exception _ nil))]
+         (if (and valor (pos? valor) (>= @saldo valor))
+           (do
+             (swap! saldo - valor)
+             (json/generate-string {:mensagem "Saque realizado com sucesso" :saldo @saldo}))
+           (json/generate-string {:erro "Saldo insuficiente ou valor inválido para saque"}))))
+
+
+(POST "/apostar" {body :body}
+      (println "Body recebido em /apostar:" body)
+      (let [evento-id (or (:evento-id body) (:eventoId body)) 
+            mercado-id (or (:mercado-id body) (:mercadoId body))
+            resultado (or (:resultado body) (get body "resultado"))
+            valor (try
+                    (Double/parseDouble (str (or (:valor body) (get body "valor"))))
+                    (catch Exception _ nil))]
+        (println "Dados processados: evento-id:" evento-id ", mercado-id:" mercado-id ", resultado:" resultado ", valor:" valor)
+        (if (and evento-id mercado-id resultado (number? valor) (> valor 0) (>= @saldo valor))
+          (do
+            (swap! saldo - valor)
+            (swap! apostas conj {:evento-id evento-id
+                                 :mercado-id mercado-id
+                                 :resultado resultado
+                                 :valor valor
+                                 :saldo-restante @saldo})
+            (println "Aposta realizada com sucesso. Novo saldo:" @saldo)
+            (json/generate-string {:mensagem "Aposta realizada com sucesso" :saldo @saldo}))
+          (do
+            (println "Erro na validação dos dados da aposta.")
+            (json/generate-string {:erro "Saldo insuficiente, valor inválido ou dados incompletos"})))))
+
+
+
+  (GET "/eventos/:torneio-id" [torneio-id]
+       (let [data (buscar-eventos torneio-id)]
+         (if data
+           (json/generate-string data)
+           (json/generate-string {:erro "Erro ao buscar eventos"}))))
+
+
+  (GET "/odds/:evento-id" [evento-id]
+       (let [odds (buscar-odds evento-id)]
+         (if odds
+           (json/generate-string odds)
+           (json/generate-string {:erro "Erro ao buscar odds"}))))
+
+
+  (GET "/consultar-apostas" []
+       (json/generate-string {:apostas @apostas})))
+
+(def app
+  (-> app-routes
+      (wrap-json-body {:keywords? true})
+      wrap-json-response
+      (wrap-cors :access-control-allow-origin [#".*"]
+                 :access-control-allow-methods [:get :put :post :options]
+                 :access-control-allow-headers ["Content-Type"])))
+
+
+
+
+(defn -main []
+  (jetty/run-jetty app {:port 8081 :join? false}))
